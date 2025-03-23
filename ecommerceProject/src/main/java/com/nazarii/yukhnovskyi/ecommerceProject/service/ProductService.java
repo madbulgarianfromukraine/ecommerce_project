a650704ff24de855de2e9d7aa69d2795fae794df
madbulgarianfromukraine <go72raj@mytum.de>
2025-03-23 14:45:14 +0100

    did some changes for springBoot to create id unique to the postgres db

--- ecommerceProject/src/main/java/com/nazarii/yukhnovskyi/ecommerceProject/service/ProductService.java
+++ ecommerceProject/src/main/java/com/nazarii/yukhnovskyi/ecommerceProject/service/ProductService.java
@@ -4,7 +4,9 @@ import com.nazarii.yukhnovskyi.ecommerceProject.model.Product;
 import com.nazarii.yukhnovskyi.ecommerceProject.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -25,5 +27,14 @@ public class ProductService {
 
     public Product getProduct(int id) {
         return repo.findById((long) id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException{
+        product.setImgName(imageFile.getOriginalFilename());
+        product.setImgType(imageFile.getContentType());
+        product.setImg(imageFile.getBytes());
+
+        return repo.save(product);
+
     }
 }
